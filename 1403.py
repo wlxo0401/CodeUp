d56ca083a4aa05a25a68d647c797bbe3535e620a
kimjitae <wlxo0401@gmail.com>
2020-08-11 17:55:13 +0900

    hi

---
+++ 1403.py
@@ -0,0 +1,18 @@
+# k개의 숫자를 입력받고 그 숫자들을 두번 출력하시오.
+
+# 입력 예) 
+# 2
+# 5 7
+# 출력 예)
+# 5
+# 7
+# 5
+# 7
+
+num = int(input())
+
+nums = list(map(int, input().split(" ")))
+
+for j in range(2):
+    for i in range(0, num, 1):
+        print(nums[i])
